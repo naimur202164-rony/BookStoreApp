baab7d0aed88a66ab8834a223f94e3d080ef5c43
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-06 20:27:51 +0600

    backend-compeleted

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-const express=require('express');
-const mongoose=require('mongoose')
-const router=require("./routes/book-routs")
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
 
-
-// Middlware
-
-app.use('/books',router);
-
-
-
-
-
-
-
-
-// connection of data-base
-// Pas:pixchiMbJJjYDwmU
-mongoose.connect(`mongodb+srv://admin:pixchiMbJJjYDwmU@cluster0.aztyb.mongodb.net/bookStore?retryWrites=true&w=majority
-
-`)
-.then(()=>console.log("Connected to the Data base"))
-.then(()=>
-app.listen(5000)
-)
-.catch((err)=>console.log(err))
+mongoose
+  .connect(
+    "mongodb+srv://admin:nUd0zMDtbX35sTL0@cluster0.5yvzr.mongodb.net/bookStore?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
--- model/Book.js
+++ model/Book.js
@@ -1,35 +1,33 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
-
-
-
-const bookSchema=new Schema({
-    name:{
-        type:String,
-        required:true,
-    },
-    author:{
-        type:String,
-        required:true,
-    },
-    discription:{
-        type:String,
-        required:true,
-    },
-    price:{
-        type:Number,
-        required:true,
-    },
-    available:{
-        type:Boolean,
-        
-    }
+const bookSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  available: {
+    type: Boolean,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
+module.exports = mongoose.model("Book", bookSchema);
 
-module.exports=mongoose.model("Book",bookSchema);
-
-
-// Books 
+// books
